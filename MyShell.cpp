05426fe14b9d5af106ba13fb7e85a1a3d1553064
A-Salty-Fish <1309032417@qq.com>
2020-10-03 14:03:27 +0800

    仿造书中代码

--- MyShell.cpp
+++ MyShell.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
-#include <vector>
 #include<string>
-
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include<errno.h>
 
 using namespace std;
 
 const string START_SIGNIAL = ">";
 
 class MyShell{
-
+public:
+    string command;
+    void RunMyShell()
+    {
+        int rtOfSon;//子进程返回值
+        while(true)
+        {
+            cout << START_SIGNIAL;
+            cin >> command;
+            if (fork() == 0)
+            {
+                // char * charCommand = new char(sizeof(char)*(command.length()+1));
+                const char * charCommand = command.c_str();
+                execlp(charCommand,charCommand);
+                perror(charCommand);
+                exit(errno);
+            }
+            else
+            {
+                wait(&rtOfSon);
+                cout << "child process return " << rtOfSon << endl;
+            }
+            
+        }
+    }
 };
+
+void main()
+{
+
+}
