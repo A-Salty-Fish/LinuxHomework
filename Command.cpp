2837e3fdc43a28cca7d766471760e7682a0c2413
A-Salty-Fish <1309032417@qq.com>
2020-10-03 15:02:07 +0800

    抽象出运行和参数部分

--- Command.cpp
+++ Command.cpp
@@ -1,28 +1,4 @@
 #include<string>
 #include<vector>
 using namespace std;
-class Command
-{
-private:
-    string cmdStr;//命令
-    vector<string> argsStrVec;//参数字符串
-public:
-    Command(string cmd = "null");//构造函数
-    void AddArgsStr(string arg);//添加参数字符串
-    int Execute();//执行
-};
 
-Command::Command(string cmd = "null")
-{
-    this->cmdStr = cmd;
-}
-
-void Command::AddArgsStr(string arg)
-{
-
-}
-
-int Command::Execute()
-{
-    return 1;
-}
--- MyShell.cpp
+++ MyShell.cpp
@@ -3,11 +3,63 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include<errno.h>
+#include<vector>
+#include <cstring>
 
 using namespace std;
 
 const string START_SIGNIAL = ">";
 const char * BUSYBOX_PATH = "./busybox-1.32.0/busybox";
+
+class Command
+{
+private:
+    vector<string> argsStrVec;//命令参数字符串
+public:
+    Command();//构造函数
+    void AddArgsStr(string arg);//添加参数字符串
+    char ** GetArgs();//生成参数char数组
+    void Execute();//执行
+};
+
+Command::Command()
+{
+}
+
+void Command::AddArgsStr(string arg)
+{
+    argsStrVec.push_back(arg);
+}
+
+char ** Command::GetArgs()
+{
+    int argsNum = argsStrVec.size();
+    char **args = (char**) malloc( sizeof(char*) * argsNum );
+    for (int i = 0; i < argsNum; i++)
+    {
+        char *arg = new char[argsStrVec[i].length() + 1];
+        strcpy(arg, argsStrVec[i].c_str());
+        args[i] = arg;
+    }
+    return args;
+}
+
+void Command::Execute()
+{
+    int rtOfSon;//子进程返回值
+    if (fork() == 0)
+    {   
+        char **_argvs = GetArgs();
+        execv(BUSYBOX_PATH,_argvs);
+        perror(BUSYBOX_PATH);
+        exit(errno);
+    }
+    else
+    {
+        wait(&rtOfSon);
+        cout << "child process return " << rtOfSon << endl;
+    }    
+}
 
 class MyShell{
 public:
@@ -19,22 +71,9 @@ public:
         {
             cout << START_SIGNIAL;
             cin >> command;
-            if (fork() == 0)
-            {
-                const char * charCommand = command.c_str();
-                char **_argvs = (char**) malloc( sizeof(char*) * 10 );
-                _argvs[0] = (char *) "ls";
-                _argvs[1] = NULL;
-                execv(charCommand,_argvs);
-                perror(charCommand);
-                exit(errno);
-            }
-            else
-            {
-                wait(&rtOfSon);
-                cout << "child process return " << rtOfSon << endl;
-            }
-            
+            Command command;
+            command.AddArgsStr("ls");
+            command.Execute();
         }
     }
 };
