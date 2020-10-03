5b8f80bfde8372f36e141cabb3a581762bf97c96
A-Salty-Fish <1309032417@qq.com>
2020-10-03 13:45:15 +0800

    初始化基础类

---
+++ MyShell.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include<string>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+using namespace std;
+
+const string START_SIGNIAL = ">";
+
+class MyShell{
+
+};
